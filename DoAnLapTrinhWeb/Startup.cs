ac5dbf26e485ced076d82632d3f935a9ce3adb4c
dendekun <111942541+dendekun@users.noreply.github.com>
2023-03-30 16:04:08 +0700

    Add project files.

---
+++ DoAnLapTrinhWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAnLapTrinhWeb.Startup))]
+namespace DoAnLapTrinhWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
